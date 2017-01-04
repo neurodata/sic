409f8fa671605300c6fa5f89484aa56a2813a274
gkiar <gkiar07@gmail.com>
2017-01-04 17:24:23 -0500

    updated reboot script

--- code/jupyter/auto_reboot.sh
+++ code/jupyter/auto_reboot.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker rm -f `docker ps | grep sic:ndmg | awk '{ print $1 }'`
-docker run -d -p 80:8888 sic:ndmg
-echo "oh hey man"
+docker rm -f `docker ps | grep sic/ndmg | awk '{ print $1 }'`
+docker run -d -p 80:8888 sic/ndmg
