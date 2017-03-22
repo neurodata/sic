56474a95bb4f34667522b66e88893d9e4a016b42
Greg Kiar <gkiar@jhu.edu>
2017-03-22 12:41:03 -0400

    updated cronjob

--- code/jupyter/auto_reboot.sh
+++ code/jupyter/auto_reboot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker rm -f `docker ps | grep sic/ndmg | awk '{ print $1 }'`
-docker run -d -p 80:8888 sic/ndmg
+docker rm -f `docker ps | grep neurodata/sic | awk '{ print $1 }'`
+docker run -d -p 80:8888 neurodata/sic:paper
